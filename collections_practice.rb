728573c11ad6a7e2a0a05abbf41fd96e4f723665
Christina Williams <christinaxtwilliams@gmail.com>
2018-09-30 14:50:52 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -5,6 +5,12 @@ def sort_array_desc(integers)
   integers.sort {|first_num, second_num| second_num <=> first_num}
 end
 def sort_array_char_count(strings)
-  strings.sort {|left, right| left.length <=> right.length}
+  stdef swap_elements(strings)
+  strings[1], strings[2] = strings[2], strings[1]
+  return strings
+endrings.sort {|left, right| left.length <=> right.length}
 end
-
+def swap_elements(strings)
+  strings[1], strings[2] = strings[2], strings[1]
+  return strings
+end
